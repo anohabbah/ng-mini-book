bcacf7a9bb2276838ecbf389dcd603d5b78f3e67
Abbah Anoh <aanoh.timwi@gmail.com>
2021-11-19 15:44:33 +0100

    Unit test the SearchComponent

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,19 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
+import {SearchService} from "@app/shared";
+import {ActivatedRoute} from "@angular/router";
+import {MockActivatedRoute} from "@app/shared/search/mocks/route";
+import {FormsModule} from "@angular/forms";
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {of} from "rxjs";
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let mockSearchService: SearchService;
+  let mockActivatedRoute: ActivatedRoute;
 
   beforeEach(async () => {
+    mockActivatedRoute = new MockActivatedRoute({ term: 'nikola'});
+
     await TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [ SearchComponent ],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+      ],
+      imports: [FormsModule, RouterTestingModule, HttpClientTestingModule],
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    mockSearchService = TestBed.inject(SearchService);
+    mockSearchService.search = jasmine.createSpy().and.returnValue(of([]));
+
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +39,17 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should search when a term is set and search() is called', () => {
+    component = fixture.componentInstance;
+    component.query = 'J';
+    component.search();
+    expect(mockSearchService.search).toHaveBeenCalledWith('J');
+  });
+
+  it('should search automatically when a term is on the URL', () => {
+    fixture.detectChanges();
+    expect(mockSearchService.search).toHaveBeenCalledWith('nikola');
   });
 });
