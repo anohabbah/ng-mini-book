73e8474a9f85f6081bf8d8efa849a784553d402c
Abbah Anoh <aanoh.timwi@gmail.com>
2021-11-19 15:51:17 +0100

    Unit test the EditComponent

--- src/app/edit/edit.component.spec.ts
+++ src/app/edit/edit.component.spec.ts
@@ -1,16 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { EditComponent } from './edit.component';
+import {EditComponent} from './edit.component';
+import {Address, Person, SearchService} from "@app/shared";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MockActivatedRoute, MockRouter} from "@app/shared/search/mocks/route";
+import {FormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {of} from "rxjs";
 
 describe('EditComponent', () => {
   let component: EditComponent;
   let fixture: ComponentFixture<EditComponent>;
 
+  let mockSearchService: SearchService;
+  let mockActivatedRoute: MockActivatedRoute;
+  let mockRouter: MockRouter;
+
   beforeEach(async () => {
+    mockActivatedRoute = new MockActivatedRoute({id: 1});
+    mockRouter = new MockRouter();
+
     await TestBed.configureTestingModule({
-      declarations: [ EditComponent ]
+      declarations: [EditComponent],
+      providers: [
+        {provide: ActivatedRoute, useValue: mockActivatedRoute},
+        {provide: Router, useValue: mockRouter}
+      ],
+      imports: [FormsModule, HttpClientTestingModule],
     })
-    .compileComponents();
+      .compileComponents();
+
+    mockSearchService = TestBed.inject(SearchService);
   });
 
   beforeEach(() => {
@@ -21,5 +41,23 @@ describe('EditComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch a single record', () => {
+    const fixture = TestBed.createComponent(EditComponent);
+    const person = new Person({id: 1, name: 'Michael Porter Jr.'});
+    person.address = new Address({city: 'Denver'});
+// mock response
+    spyOn(mockSearchService, 'get').and.returnValue(of(person));
+// initialize component
+    fixture.detectChanges();
+// verify service was called
+    expect(mockSearchService.get).toHaveBeenCalledWith(1);
+// verify data was set on component when initialized
+    const editComponent = fixture.componentInstance;
+    expect(editComponent.person.address.city).toBe('Denver');
+// verify HTML renders as expected
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h3').innerHTML).toBe('Michael Porter Jr.');
   });
 });
