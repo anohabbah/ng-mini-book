a2e86d7f51034b8252762cf0aa2bab1ec1c13450
Abbah A <25776788+anohabbah@users.noreply.github.com>
2021-11-20 09:40:59 +0100

    updates

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -43,7 +43,7 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('signIn', (username, password) => {
-  cy.get('input[name=username]').type(username);
+  cy.get('input[name=email]').type(username);
   cy.get('input[name=password]').type(password);
   cy.get('button[type=submit]').first().click();
 })
